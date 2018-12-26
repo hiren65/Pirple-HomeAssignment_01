2cf241ed8dcf5e943cd2c2fb0b5bc674ec65bfbb
hirenkumar <hirenkumar@me.com>
2018-12-26 15:06:55 +1100

    reqested url parsed and trimmed

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var url = require('url');
 
 //create server
 var server = http.createServer(function(req,res){
+  //get parsed string using module url.parse()
+  var parsedUrl = url.parse(req.url,true);
+  //get path
+  var path = parsedUrl.pathname;
+  //using regex trimme path
+  var trimmedPath = path.replace(/^\/+|\/+$/g,"");
 
   res.end("Hello World");
+  console.log("trimmedPath " + trimmedPath);
 });
 //server response
 server.listen(3000,function(){
